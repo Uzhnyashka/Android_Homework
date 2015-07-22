97d3364ecd022338429a5970e34d517a354b638c
EgorBobyk <egor4k.964@gmail.com>
2015-07-22 23:02:46 +0300

    Basic map and basic custom zoom

--- app/src/main/java/com/example/bobyk/android_homework/MainActivity.java
+++ app/src/main/java/com/example/bobyk/android_homework/MainActivity.java
@@ -1,39 +1,63 @@
 package com.example.bobyk.android_homework;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MainActivity extends ActionBarActivity {
+/**
+ * Created by bobyk on 22/07/15.
+ */
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, View.OnClickListener{
 
+    private GoogleMap mGoogleMap;
+    private Button btnZoomIn, btnZoomOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initMap();
     }
 
+    private void findViews(){
+        btnZoomIn   = (Button) findViewById(R.id.btn_ZoomIn);
+        btnZoomOut  = (Button) findViewById(R.id.btn_ZoomOut);
+
+        btnZoomIn   .setOnClickListener(this);
+        btnZoomOut  .setOnClickListener(this);
+    }
+
+    private void initMap(){
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.googleMapFragment);
+        mapFragment.getMapAsync(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        findViews();
+        mGoogleMap.setMyLocationEnabled(true);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
